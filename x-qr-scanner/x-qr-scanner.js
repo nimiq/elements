88ee9cdc9febe9476dd835961d5e9999179573e1
robinlinus <robinlinus@users.noreply.github.com>
2018-01-06 22:30:07 +0100

    Add x-identicon, x-qr-scanner
    Former-commit-id: e9f3936e682a1edcace8b335469f2eeb82de5601

---
+++ x-qr-scanner/x-qr-scanner.js
@@ -0,0 +1,19 @@
+class XQrScanner extends XElement {
+    onCreate() {
+        const video = this.$('video');
+        const canvas = this.$('canvas');
+        this._scanner = new QrScanner(video, canvas, this._onDecode.bind(this));
+    }
+
+    _onDecode(data) {
+        this.fire('x-decoded', data);
+    }
+
+    set active(active) {
+        this._scanner.active = active;
+    }
+
+    setGrayscaleWeights(red, green, blue) {
+        this._scanner.setGrayscaleWeights(red, green, blue);
+    }
+}
