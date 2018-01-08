2b1e1c1b88a3d2ac4406d27f6adbb878bd1666d7
danimoh <github.danimoh@neverbox.com>
2018-01-08 18:08:09 +0100

    qr scanner overlay
    Former-commit-id: b0a01f8a98759bbe21da137303ed3eadbc44689b

--- x-qr-scanner/x-qr-scanner.js
+++ x-qr-scanner/x-qr-scanner.js
@@ -1,11 +1,14 @@
 class XQrScanner extends XElement {
     onCreate() {
         const $video = this.$('video');
+        this.$qrOverlay = this.$('[qr-overlay]');
         this.$fileUpload = this.$('input[type="file"]');
         this.$fileUpload.addEventListener('change', () => this._onFileSelected());
         // requires https://github.com/nimiq/qr-scanner
         this._scanner = new QrScanner($video, result => this.fire('x-decoded', result));
 
+        this._positionOverlay();
+        window.addEventListener('resize', () => this._positionOverlay());
     }
 
     set active(active) {
@@ -27,8 +30,21 @@ class XQrScanner extends XElement {
             .catch(() => this.fire('x-error', 'No QR code found.'));
     }
 
+    _positionOverlay() {
+        const scannerHeight = this.$el.offsetHeight;
+        const scannerWidth = this.$el.offsetWidth;
+        const smallerDimension = Math.min(scannerHeight, scannerWidth);
+        const overlaySize = 2/3 * smallerDimension; /* not always the accurate size of the sourceRect for QR
+        detection (e.g. if video is landscape and screen portrait) but looks nicer in UI */
+        this.$qrOverlay.style.width = overlaySize + 'px';
+        this.$qrOverlay.style.height = overlaySize + 'px';
+        this.$qrOverlay.style.top = ((scannerHeight - overlaySize) / 2) + 'px';
+        this.$qrOverlay.style.left = ((scannerWidth - overlaySize) / 2) + 'px';
+    }
+
     html(){
         return `<video muted autoplay playsinline width="600" height="600"></video>
+                <div qr-overlay></div>
                 <label icon-upload><input type="file"></label>`;
     }
 }
