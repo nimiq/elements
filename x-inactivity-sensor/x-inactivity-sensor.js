7d87c04cf9b7ad58450ffed556533b0f52226959
danimoh <github.danimoh@neverbox.com>
2018-01-14 15:36:08 +0100

    x-activity-sensor: reset on input events
    Former-commit-id: 18019f3dd7a16cc0ba633610fd9b0589b8aeb3e9

--- x-inactivity-sensor/x-inactivity-sensor.js
+++ x-inactivity-sensor/x-inactivity-sensor.js
@@ -1,13 +1,16 @@
 class XInactivitySensor extends XElement {
 
-    static get NO_INPUT_TIME() { return 3000 } // in seconds 
-    static get TAB_INVISIBLE_TIME() { return 1000 } // in seconds 
+    static get NO_INPUT_TIME() { return 3000 } // in seconds
+    static get TAB_INVISIBLE_TIME() { return 1000 } // in seconds
 
     onCreate() {
+        this._inactive = this._inactive.bind(this);
         document.addEventListener('visibilitychange', e => this._visibilityChange());
         window.addEventListener('popstate', () => this.reset());
         window.addEventListener('touchstart', () => this.reset());
         window.addEventListener('mousemove', () => this.reset());
+        window.addEventListener('input', () => this.reset());
+        window.addEventListener('change', () => this.reset());
     }
 
     _visibilityChange() {
@@ -21,7 +24,7 @@ class XInactivitySensor extends XElement {
     reset(seconds) {
         clearTimeout(this._timeout);
         seconds = seconds || XInactivitySensor.NO_INPUT_TIME;
-        this._timeout = setTimeout(() => this._inactive(), seconds * 1000);
+        this._timeout = setTimeout(this._inactive, seconds * 1000);
     }
 
     _inactive() {
@@ -30,3 +33,4 @@ class XInactivitySensor extends XElement {
 }
 
 // Todo: compute time diff on visibility change because timers might stop when tab inactive
+// Todo: check deviceorientation and devicemotion events for mobile
