0739975017e1b6280cc323fc0146e068d12a76cb
robinlinus <robinlinus@users.noreply.github.com>
2018-01-16 10:27:59 -0600

    Add x-nimiq-api
    Former-commit-id: 93d9fec426592799628b0764e02ac5e3edc4e0bf

---
+++ x-nimiq-api/x-nimiq-api.js
@@ -0,0 +1,32 @@
+class XNimiqApi extends XElement {
+    onCreate() {
+        const connect = this.$el.hasAttribute('connect');
+        this._api = new NimiqApi(connect, this);
+    }
+}
+
+class NimiqApi extends NanoApi {
+    constructor(connect, element) {
+        super(connect);
+        this._element = element;
+    }
+    onInitialized() {
+        this._element.fire('x-api-ready', this);
+    }
+
+    onConsensusEstablished() {
+        this.fire('x-consensus-establised', this.address);
+    }
+
+    onAddressChanged(address) {
+        this._element.fire('x-account', address);
+    }
+
+    onBalanceChanged(balance) {
+        this._element.fire('x-balance', balance);
+    }
+
+    onTransactionReceived(sender, value, fee) {
+        this._element.fire('x-transaction', { sender: sender, value: value, fee: fee });
+    }
+}
