a0d46038bda0c13b088039262898d269a5e20560
robinlinus <robinlinus@users.noreply.github.com>
2018-01-21 21:33:08 -0600

    Use ‘library’ instead of ‘libraries’
    Former-commit-id: 41cdcaac3bbd9192bafde5668a3e1c985df4af7a

--- x-qr-scanner/x-qr-scanner.js
+++ x-qr-scanner/x-qr-scanner.js
@@ -1,4 +1,4 @@
-import XElement from "/libraries/x-element/x-element.js";
+import XElement from "/library/x-element/x-element.js";
 import QrScanner from "/qr-scanner/qr-scanner.min.js";
 
 export default class XQrScanner extends XElement {
