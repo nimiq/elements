08dde35c3375a5136bfda172c46a3e6d3c6ffcdc
robinlinus <robinlinus@users.noreply.github.com>
2018-01-26 03:18:10 -0600

    Add x-address-file-input

---
+++ x-address-file-input/x-address-file-input.js
@@ -0,0 +1,14 @@
+import XQrFileInput from '../x-qr-file-input/x-qr-file-input.js';
+import Nimiq from '/library/nano-api/nano-api.js';
+
+export default class XAddressFileInput extends XQrFileInput {
+    _validate(address) {
+        const isValid = Nimiq.validateAddress(address);
+        if(isValid) return true;
+        this._onInvalid('No Address in QR Code!');
+    }
+}
+// Todo: [low priority] handle if user selects a backup file 
+	// 0: Create Nimiq.validateBackup
+	// 1: tell user this is not an address but an backup file 
+	// 2: ask user to send money to that account ? 
