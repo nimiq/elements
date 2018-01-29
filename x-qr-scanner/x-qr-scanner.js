b3ff06632891c648dcda89d6c95e014a3b6b14c6
danimoh <github.danimoh@neverbox.com>
2018-01-29 14:26:00 -0600

    x-qr-scanner: fire only every 7s

--- x-qr-scanner/x-qr-scanner.js
+++ x-qr-scanner/x-qr-scanner.js
@@ -2,16 +2,23 @@ import XElement from '/library/x-element/x-element.js';
 import QrScanner from '/library/qr-scanner/qr-scanner.min.js';
 
 export default class XQrScanner extends XElement {
+    static get REPORT_FREQUENCY() {
+        return 7000; // report the same result only every 7s
+    }
+
     html() {
         return `
             <video muted autoplay playsinline width="600" height="600"></video>
             <x-qr-scanner-overlay></x-qr-scanner-overlay>
         `;
     }
+
     styles() { return ['x-qr-scanner'] }
 
     onCreate() {
         const $video = this.$('video');
+        this._lastResult = null;
+        this._lastResultTime = 0;
         this._scanner = new QrScanner($video, result => this._onResult(result));
         this.$qrOverlay = this.$('x-qr-scanner-overlay');
         window.addEventListener('resize', () => this._positionOverlay());
@@ -27,7 +34,10 @@ export default class XQrScanner extends XElement {
     }
 
     _onResult(result) {
-        if (!this._validate(result)) return;
+        if ((result === this._lastResult && Date.now() - this._lastResultTime < XQrScanner.REPORT_FREQUENCY)
+            || !this._validate(result)) return;
+        this._lastResult = result;
+        this._lastResultTime = Date.now();
         const eventName = this.__tagName.replace('scanner', 'scan');
         this.fire(eventName, result);
     }
@@ -43,8 +53,8 @@ export default class XQrScanner extends XElement {
         const scannerWidth = $parent.offsetWidth;
         const smallerDimension = Math.min(scannerHeight, scannerWidth);
         const overlaySize = Math.ceil(2 / 3 * smallerDimension);
-        /* not always the accurate size of the sourceRect for QR
-               detection (e.g. if video is landscape and screen portrait) but looks nicer in the UI */
+        // not always the accurate size of the sourceRect for QR detection (e.g. if video is landscape and
+        // screen portrait) but looks nicer in the UI.
         this.$qrOverlay.style.width = overlaySize + 'px';
         this.$qrOverlay.style.height = overlaySize + 'px';
         this.$qrOverlay.style.top = ((scannerHeight - overlaySize) / 2) + 'px';
@@ -54,7 +64,4 @@ export default class XQrScanner extends XElement {
     _validate() { return true; /* abstract method */ }
 }
 
-
-
-// Todo: [Daniel] event should fire only once every 7s for the same result
 // Todo: [Daniel] [low priority] can we detect if camera exists without asking for permission?
