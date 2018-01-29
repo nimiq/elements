37b7528ce23d0db5a2f04550973920c3011ff630
robinlinus <robinlinus@users.noreply.github.com>
2018-01-29 00:30:42 -0600

    backup-file-import: use screens instead of slides

--- screen-backup-file-import/screen-backup-file-import.js
+++ screen-backup-file-import/screen-backup-file-import.js
@@ -1,46 +1,81 @@
 import XScreen from '../x-screen/x-screen.js';
-import XSlides from '../x-slides/x-slides.js';
+import XScreenFit from '../x-screen/x-screen-fit.js';
 import XWalletBackupImport from '../x-wallet-backup-import/x-wallet-backup-import.js';
-import XSuccessMark from '../x-success-mark/x-success-mark.js';
 import XPasswordInput from '../x-password-input/x-password-input.js';
+import ScreenSuccess from '../screen-success/screen-success.js';
+import ScreenLoading from '../screen-loading/screen-loading.js';
 
 export default class ScreenBackupFileImport extends XScreen {
     html() {
         return `
         <h1>Import Backup File</h1>
         <x-slides>
-            <x-slide>
-                <h2 secondary>Select a backup file to import an account</h2>
-                <x-wallet-backup-import></x-wallet-backup-import>
-            </x-slide>
-            <x-slide>
-                <h2 secondary>Enter the password to unlock this backup</h2>
-                <x-password-input></x-password-input>
-                <x-grow></x-grow>
-                <button disabled="yes">Unlock</button>
-            </x-slide>
-            <x-slide>
-                <x-loading-animation></x-loading-animation>
-                <h2>Unlocking the Backup</h2>
-            </x-slide>
-            <x-slide>
-                <x-success-mark></x-success-mark>
-                <h2>Account Imported</h2>
-            </x-slide>
+            <screen-backup-file-import-intro></screen-backup-file-import-intro>
+            <screen-backup-file-import-password></screen-backup-file-import-password>
+            <screen-loading>Unlocking the Backup</screen-loading>
+            <screen-success>Account Imported</screen-success>
         </x-slides>
         `;
     }
 
-    styles() { return ['x-screen'] }
-
-    children() { return [XSlides, XWalletBackupImport, XSuccessMark, XPasswordInput] }
+    children() { return [ScreenBackupFileImportIntro, ScreenBackupFileImportPassword, ScreenLoading, ScreenSuccess] }
 
     onCreate() {
+        this.addEventListener('x-success', e => _onSuccess(e));
         this.addEventListener('x-backup-import', e => this._onWalletImport(e));
+        this.addEventListener('x-password-input', e => this._onPasswordInput(e));
+    }
+
+    _onWalletImport(e) {
+        e.stopPropagation();
+        this._encryptedKey = e.detail;
+        this.goTo('password');
+    }
+
+    _onPasswordInput(e){
+        const password = e.detail;
+        const result = { password: password, encryptedKey: this._encryptedKey }
+        this.fire('x-decrypt-backup', result);
+        this.goTo('loading');
+    }
+
+    async onPasswordIncorrect() {
+        await this.back();
+        this.$screenBackupFileImportPassword.$passwordInput._onInvalid();
+    }
+}
+
+class ScreenBackupFileImportIntro extends XScreenFit {
+    html() {
+        return `
+            <h2 secondary>Select a backup file to import an account</h2>
+            <x-wallet-backup-import></x-wallet-backup-import>
+        `
+    }
+
+    children() { return [XWalletBackupImport] }
+
+    get route() { return 'intro' }
+}
+
+class ScreenBackupFileImportPassword extends XScreenFit {
+    html() {
+        return `
+            <h2 secondary>Enter the password to unlock this backup</h2>
+            <x-password-input></x-password-input>
+            <x-grow></x-grow>
+            <button disabled="yes">Unlock</button>
+        `
+    }
+
+    get route() { return 'password' }
+
+    children() { return [XPasswordInput] }
+
+    onCreate() {
+        this.addEventListener('x-password-input-valid', e => this._validityChanged(e.detail));
         this.$button = this.$('button');
         this.$button.addEventListener('click', e => this._onPasswordInput(e));
-        this.addEventListener('x-password-input', e => this._onPasswordInput());
-        this.addEventListener('x-password-input-valid', e => this._validityChanged(e.detail));
     }
 
     _validityChanged(valid) {
@@ -50,36 +85,16 @@ export default class ScreenBackupFileImport extends XScreen {
             this.$button.setAttribute('disabled', true);
     }
 
-    _onBeforeEntry() {
-        this.$slides._onResize();
-        this.$slides.jumpTo(0);
-    }
-
-    async _onWalletImport(e) {
-        e.stopPropagation();
-        this._encryptedKey = e.detail;
-        await this.$slides.next()
+    _onEntry() {
         this.$passwordInput.focus();
     }
 
-    _onPasswordInput() {
-        const password = this.$passwordInput.value;
-        const result = { password: password, encryptedKey: this._encryptedKey }
-        this.fire('x-decrypt-backup', result);
-        this.$slides.next();
-    }
-
-    async onPasswordIncorrect() {
-        await this.$slides.prev();
-        this.$passwordInput.onWrong();
-        this.$button.setAttribute('disabled', true);
-    }
-
-    async onSuccess() {
-        await this.$slides.next();
-        await this.$successMark.animate();
+    _onPasswordInput(e) {
+        this.fire('x-password-input', this.$passwordInput.value);
     }
 }
+
+
 
 // Todo: warn user upfront that importing a different account deletes the current account
 // Todo: [low priority] support multiple accounts at once
