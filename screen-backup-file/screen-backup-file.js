9044d448bc9ef2b89bef21f1f4a0b9d643e235e2
Don Maxito <mh@expertsinside.com>
2018-01-30 17:12:12 -0600

    screen-backup-file: added half-working x-indicator

--- screen-backup-file/screen-backup-file.js
+++ screen-backup-file/screen-backup-file.js
@@ -15,6 +15,7 @@ export default class ScreenBackupFile extends XScreen {
                 <screen-download-recovery></screen-download-recovery>
                 <screen-complete></screen-complete>
             </x-slides>
+            <x-indicator></x-indicator>
             <screen-no-password-warning></screen-no-password-warning>
             `
     }
@@ -30,7 +31,17 @@ export default class ScreenBackupFile extends XScreen {
     }
 
     _onEntry() {
+        this._prepareIndicators();
         this.goTo('password');
+    }
+
+    _prepareIndicators() {
+        this.$indicator = this.$('x-indicator');
+        this.$slides = this.$('x-slides');
+        for (let i = 0; i < this.$slides.children.length; i++) {
+            this.$indicator.appendChild(document.createElement('x-dot'));
+        }
+        this.$indicator = this.$indicator.childNodes;
     }
 
     async reset() { }
