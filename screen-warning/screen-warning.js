c2139fd29af01456c2529b05a5d5838f92f4af49
robinlinus <robinlinus@users.noreply.github.com>
2018-02-01 02:36:04 -0600

    Add screen-warning

---
+++ screen-warning/screen-warning.js
@@ -0,0 +1,17 @@
+import XScreenFit from '../x-screen/x-screen-fit.js';
+export default class ScreenWarning extends XScreenFit {
+    html() {
+        return `
+			<x-warning x-grow>
+				<i icon-warning></i>
+				<h2>Important Notice</h2>
+				<div content>Warning!</div>
+			</x-warning>
+			<button>I Understand</button>
+		`;
+    }
+
+    onCreate() {
+        this.$('button').addEventListener('click', e => this.fire('x-warning-complete'));
+    }
+}
