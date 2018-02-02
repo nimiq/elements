68db9d44920c1f9afda5f2c8685c65b98a0e930d
Don Maxito <mh@expertsinside.com>
2018-02-02 16:56:07 -0600

    refactoring screen-backup-phrase

--- screen-backup-phrase/screen-backup-phrase.js
+++ screen-backup-phrase/screen-backup-phrase.js
@@ -1,5 +1,4 @@
 import XScreen from '../x-screen/x-screen.js';
-import XSlides from '../x-slides/x-slides.js';
 import XPrivacyAgent from '../x-privacy-agent/x-privacy-agent.js';
 import XMnemonicPhrase from '../x-mnemonic-phrase/x-mnemonic-phrase.js';
 
@@ -8,43 +7,55 @@ export default class ScreenBackupPhrase extends XScreen {
         return `
             <h1>Backup your Recovery Phrase</h1>
             <x-slides>
-                <x-slide>
-                    <h2 secondary>First make sure your enviroment is safe.</h2>
-                    <x-privacy-agent></x-privacy-agent>
-                    <x-grow></x-grow>
-                </x-slide>
-                <x-slide>
-                    <h2 secondary>Write down the following 24 words to recover your account later</h2>
-                    <x-mnemonic-phrase></x-mnemonic-phrase>
-                    <x-grow></x-grow>
-                    <a href="#backup-phrase-validate" button>Validate</a>
-                </x-slide>
+                <screen-privacy></screen-privacy>
+                <screen-phrase></screen-phrase>
             </x-slides>
             `;
     }
-    children() { return [XSlides, XMnemonicPhrase, XPrivacyAgent] }
-
-    onCreate() {
-        this.addEventListener('x-surrounding-checked', e => this._onSurroundingChecked())
-    }
-
-    _onSurroundingChecked() {
-        this.$slides.next();
-        this.$mnemonicPhrase.animateEntry();
-    }
-
-    _animateButton() {
-        this.$('[button]').classList.add('fade-in');
-    }
+    children() { return [ScreenPrivacy, ScreenPhrase] }
+    
+    _getDefaultScreen() { return this._childScreens['privacy']; }
 
     set privateKey(privateKey) {
-        this.$slides.jumpTo(0);
-        this.$mnemonicPhrase.privateKey = privateKey;
+        this.$screenPhrase.$mnemonicPhrase.privateKey = privateKey;
+    }
+}
+
+class ScreenPrivacy extends XScreen {
+    html() {
+        return `
+            <h2 secondary>First make sure your enviroment is safe.</h2>
+            <x-privacy-agent></x-privacy-agent>
+            <x-grow></x-grow>
+            `;
     }
 
-    _onBeforeEntry(){
-        this.$slides._resize();
+    children() { return [XPrivacyAgent] }
+
+    get route() { return 'privacy' }
+
+    onCreate() {
+        this.addEventListener('x-surrounding-checked', e => this.goTo('phrase'));
     }
+}
+
+class ScreenPhrase extends XScreen {
+    html() {
+        return `
+            <h2 secondary>Write down the following 24 words to recover your account later</h2>
+            <x-mnemonic-phrase></x-mnemonic-phrase>
+            <x-grow></x-grow>
+            <a href="#backup-phrase-validate" button fade-in>Validate</a>
+            `;
+    }
+
+    children() { return [XMnemonicPhrase] }
+
+    get route() { return 'phrase' }
+
+    /*_animateButton() {
+        this.$('[button]').classList.add('fade-in');
+    }*/
 }
 
 // Todo: [low priority] add warning "screenshots are not safe" ?
