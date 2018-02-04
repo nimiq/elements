b61f595d23648cc00ae2098dffe4dd189330619a
robinlinus <robinlinus@users.noreply.github.com>
2018-02-04 15:03:25 -0600

    screen-accounts: finish v1

--- screen-accounts/screen-accounts.js
+++ screen-accounts/screen-accounts.js
@@ -1,15 +1,18 @@
-import XScreen from '../x-screen/x-screen.js';
-export default class ScreenAccounts extends Screen {
+import XScreenFit from '../x-screen/x-screen-fit.js';
+import XElement from '/library/x-element/x-element.js';
+import XIdenticon from '../x-identicon/x-identicon.js';
+import XAddress from '../x-address/x-address.js';
+export default class ScreenAccounts extends XScreenFit {
     html() {
         return `
-			<x-accounts-list></x-accounts-list>
-			<button>Create Account</button>
-		`
+            <x-accounts-list></x-accounts-list>
+            <button>Create Account</button>
+        `
     }
 
     onCreate() {
-        this.$button = this.$('button');
         this.$accountsList = this.$('x-accounts-list');
+        this.$('button').addEventListener('click', e => this._onCreateAccount());
     }
 
     set accounts(accounts) {
@@ -18,17 +21,35 @@ export default class ScreenAccounts extends Screen {
 
     _createAccount(account) {
         const xAccount = XAccount.createElement();
-        xAccount.address = account.address;
+        xAccount.address = account;
         this.$accountsList.appendChild(xAccount.$el);
+    }
+
+    _onCreateAccount() {
+        this.fire('x-create-account');
     }
 }
 
 class XAccount extends XElement {
     html() {
         return `
-			<x-identicon></x-identicon>
-			<x-address></x-address>
-		`
+            <x-identicon></x-identicon>
+            <x-address></x-address>
+        `
     }
     children() { return [XIdenticon, XAddress] }
+
+    onCreate() {
+        this.$el.addEventListener('click', e => this._onAccountSelected())
+    }
+
+    set address(address) {
+        this.$identicon.address = address;
+        this.$address.address = address;
+        this._address = address;
+    }
+
+    _onAccountSelected() {
+        this.fire('x-account-selected', this._address);
+    }
 }
