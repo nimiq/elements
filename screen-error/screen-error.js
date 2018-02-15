69290492376f740705dcd98377003c6f35385126
sisou <hello@soerenschwert.de>
2018-02-15 16:46:30 -0600

    Add functionality to show an action link on screen-error

--- screen-error/screen-error.js
+++ screen-error/screen-error.js
@@ -5,11 +5,13 @@ export default class ScreenError extends XScreen {
             <img src="/elements/screen-error/screen-error.svg">
             <h1>Error</h1>
             <h2>Unfortunately we don't know the reason</h2>
+            <a button href="" class="small hidden"></a>
         `
     }
 
     onCreate() {
         this.$h2 = this.$('h2');
+        this.$button = this.$('[button]');
         const message = this.$el.getAttribute('message');
         if (message !== undefined) {
             this.show(message);
@@ -18,5 +20,11 @@ export default class ScreenError extends XScreen {
 
     show(message) {
         this.$h2.textContent = message;
+    }
+
+    setLink(href, text) {
+        this.$button.href = href;
+        this.$button.textContent = text;
+        this.$button.classList.remove('hidden');
     }
 }
