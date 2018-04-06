e0af15ed0dddbc867464e4bfc22160fc5ff01af6
danimoh <github.danimoh@neverbox.com>
2018-04-06 16:48:18 -0600

    x-ledger-ui-modal: auto hide modal after getConfirmedAddress and signTransaction

--- x-ledger-ui/x-ledger-ui-modal.js
+++ x-ledger-ui/x-ledger-ui-modal.js
@@ -2,19 +2,65 @@ import MixinModal from "/elements/mixin-modal/mixin-modal.js";
 import XLedgerUi from "./x-ledger-ui.js";
 
 export default class XLedgerUiModal extends MixinModal(XLedgerUi) {
-   styles() {
-       return [ ...super.styles(), 'nimiq-dark' ];
-   }
+    styles() {
+        return [ ...super.styles(), 'nimiq-dark' ];
+    }
 
-   onHide() {
-       this.cancelRequests();
-   }
+    onHide() {
+        this.cancelRequests();
+    }
 
-   _showInstructions(type, text) {
-       if ((type && type !== 'none') || text) {
-           this.show();
-       }
-       super._showInstructions(type, text);
-   }
+    _showInstructions(type, text) {
+        if ((type && type !== 'none') || text) {
+            this.show();
+        }
+        super._showInstructions(type, text);
+    }
+
+    cancelRequests() {
+        super.cancelRequests();
+        this.hide();
+    }
+
+/*  // TODO hiding modal for getAddress, confirmAddress and getPublicKey shouldn't be done this way, as they also get
+    // called within getConfirmedAddress and signTransaction
+    async getAddress() {
+        try {
+            return await super.getAddress();
+        } finally {
+            this.hide();
+        }
+    }
+
+    async confirmAddress(userFriendlyAddress) {
+        try {
+            return await super.confirmAddress(userFriendlyAddress);
+        } finally {
+            this.hide();
+        }
+    }
+
+    async getPublicKey() {
+        try {
+            return await super.getPublicKey();
+        } finally {
+            this.hide();
+        }
+    }*/
+
+    async getConfirmedAddress() {
+        try {
+            return await super.getConfirmedAddress();
+        } finally {
+            this.hide();
+        }
+    }
+
+    async signTransaction(transaction) {
+        try {
+            return await super.signTransaction(transaction);
+        } finally {
+            this.hide();
+        }
+    }
 }
-// TODO close modal on success
