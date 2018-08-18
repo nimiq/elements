6d4db1629aa3f2b492ec1840f7e8829cb3a4ae65
Smitty <smittyvb@gmail.com>
2018-08-18 12:40:56 +0000

    Use strings for x-address

--- x-address/x-address.js
+++ x-address/x-address.js
@@ -2,6 +2,7 @@ import XElement from '/libraries/x-element/x-element.js';
 import Clipboard from '/libraries/nimiq-utils/clipboard/clipboard.js';
 import XToast from '../../secure-elements/x-toast/x-toast.js';
 import ValidationUtils from '/libraries/secure-utils/validation-utils/validation-utils.js';
+import { getString } from '../strings.js';
 
 export default class XAddress extends XElement {
     styles() { return ['x-address'] }
@@ -14,7 +15,7 @@ export default class XAddress extends XElement {
 
     _onCopy() {
         Clipboard.copy(this.$el.textContent);
-        XToast.show('Account number copied to clipboard!')
+        XToast.show(getString('address_copied'))
     }
 
     set address(address) {
