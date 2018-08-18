a652ece27ecb0ce4f931efbef43bcfe526ff5810
Smitty <smittyvb@gmail.com>
2018-08-18 21:58:47 +0000

    Use strings for intro slides

--- x-education-slides/x-education-slide-point-of-nimiq.js
+++ x-education-slides/x-education-slide-point-of-nimiq.js
@@ -1,30 +1,32 @@
 import XEducationSlide from './x-education-slide.js';
+import { getString } from '../strings.js';
 
 export default class XEducationSlidePointOfNimiq extends XEducationSlide {
     html() {
         return `
             <h1 class="modal-header">
-                Why can't Nimiq Safe do those things?
+                ${getString('point_of_nimiq_slide_title')}
             </h1>
             <div class="modal-body">
                 <div class="has-side-image">
                     <div class="side-image-point-of-nimiq"></div>
                     <div>
                         <ul>
-                            <li>Because that is the point of decentralization and the blockchain.</li>
-                            <li>You don't have to rely on your bank, government, or anyone else when you want to move your funds.</li>
-                            <li>You don't have to rely on the security of an exchange or bank to keep your funds safe.</li>
-                            <li>If you don't find these things valuable, ask yourself why you think the blockchain and cryptocurrencies are valuable. 😉</li>
+                            <li>${getString('point_of_nimiq_slide_reason_list_1')}</li>
+                            <li>${getString('point_of_nimiq_slide_reason_list_2')}</li>
+                            <li>${getString('point_of_nimiq_slide_reason_list_3')}</li>
+                            <li>${getString('point_of_nimiq_slide_reason_list_4')}</li>
+
                         </ul>
                     </div>
                 </div>
 
                 <div class="button-bar">
-                    <button back>But... Why?</button>
-                    <button next>Protect your Funds</button>
+                    <button back>${getString('point_of_nimiq_slide_back')}</button>
+                    <button next>${getString('point_of_nimiq_slide_next')}</button>
                 </div>
                 <div class="spacing-top center">
-                    <a secondary class="skip">Skip AT YOUR OWN RISK</a>
+                    <a secondary class="skip">${getString('edu_slides_skip')}</a>
                 </div> 
             </div>
         `;
